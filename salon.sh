92c7ea7787201ceea05932d9791b04e8c37dc8e4
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-02 23:56:58 +0100

    Update salon.sh to correct database name and enhance welcome message; add main menu function for service display

--- salon.sh
+++ salon.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 # chmod +x salon.sh
 
-# PSQL="psql -X --username=freecodecamp --dbname=bikes --no-align --tuples-only -c"
-PSQL="psql -X --username=postgres --dbname=bikes --no-align --tuples-only -c" 
+# PSQL="psql -X --username=freecodecamp --dbname=salon --no-align --tuples-only -c"
+PSQL="psql -X --username=postgres --dbname=salon --no-align --tuples-only -c"
 
-echo -e "\n~~~~~ MY SALON ~~~~~\n"
+# Display welcome message
+echo -e "\n~~~~~ THE SALON ~~~~~\n"
+
+# The main menu function
+DISPLAY_SERVICES() {
+    # if an argument is passed to this function, display it
+    if [[ $1 ]]; then
+        echo -e "\n$1"
+    fi
+
+    # Display main menu options
+    echo -e "Welcome to the salon, how can I help you?\n"
+    echo "These are the services we offer:"
+
+    SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+    echo "$SERVICES" | while IFS="|" read SERVICE_ID SERVICE_NAME; do
+        echo "$SERVICE_ID) $SERVICE_NAME"
+    done
+}
+
+# Start the script by calling the main menu
+DISPLAY_SERVICES
