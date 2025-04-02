f1d722d4711ba1b0571cad113a7f9c7f7b634f74
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-02 21:31:22 +0100

    Add initial project setup for Salon Appointment Scheduler with database configuration and Bash script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Salon Appointment Scheduler
+
+## About
+
+- For this project, you will create an interactive Bash program that uses PostgreSQL to track the customers and appointments for your salon.
---
+++ salon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chmod +x salon.sh
+
+# PSQL="psql -X --username=freecodecamp --dbname=bikes --no-align --tuples-only -c"
+PSQL="psql -X --username=postgres --dbname=bikes --no-align --tuples-only -c" 
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
