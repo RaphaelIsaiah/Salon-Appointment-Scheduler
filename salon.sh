755fad65c1351d743f5cbf870a45fe7a1791b8b2
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-04 05:36:27 +0100

    Enhance service display in salon.sh with user selection validation and improved welcome message

--- salon.sh
+++ salon.sh
@@ -14,15 +14,28 @@ DISPLAY_SERVICES() {
         echo -e "\n$1"
     fi
 
-    # Display main menu options
+    # Welcome the customer and display the available services
     echo -e "Welcome to the salon, how can I help you?\n"
-    echo "These are the services we offer:"
+    echo "Which service would you like today?"
 
     SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
     echo "$SERVICES" | while IFS="|" read SERVICE_ID SERVICE_NAME; do
         echo "$SERVICE_ID) $SERVICE_NAME"
     done
+
+    # read the user's selection
+    read SERVICE_ID_SELECTED
+
+    # validate user's selection
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+
+    if [[ -z $SERVICE_NAME ]]; then
+        DISPLAY_SERVICES "I could not find that service. What would you like today?"
+    else
+        GET_CUSTOMER_INFO
+    fi
+
 }
 
-# Start the script by calling the main menu
+# Start the script by calling the display services function
 DISPLAY_SERVICES
